0f8bf6c3d94694f919a216eb7781c0515ae28117
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-05-02 19:56:13 +0100

    blogdetails

--- src/blogDetails.js
+++ src/blogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./usefetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -8,7 +9,18 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory
+
+const handleClick =()=>{
+  fetch('http://localhost:8000/blogs/' + blog.id,{
+    method: 'DELETE'
+  }).then(()=>{
+history.push('/');
+  })
+}
+
   return (
+    
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
@@ -17,6 +29,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
--- src/create.js
+++ src/create.js
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("");
+  const [errFlag, setErrFlag] = useState(false);
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+
+    console.log(errFlag);
+    if (errFlag) {
+      return false;
+    }
 
     setIsPending(true);
 
@@ -19,7 +27,29 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsPending(false);
+      // history.go(-1);
+      history.push('/')
     });
+
+  };
+
+  const validateField = (v, type) => {
+    //console.log(v.trim().length <= 0);
+    if (v.trim().length <= 0) {
+      setErrFlag(true);
+      if (type === "Title") {
+        return setTitle(v);
+      } else {
+        return setBody(v);
+      }
+    }
+    setErrFlag(false);
+
+    if (type === "Title") {
+      return setTitle(v);
+    } else {
+      return setBody(v);
+    }
   };
 
   return (
@@ -32,14 +62,14 @@ const Create = () => {
           type="text"
           required
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => validateField(e.target.value, "Title")}
         />
 
         <label>Blog body:</label>
         <textarea
           required
           value={body}
-          onChange={(e) => setBody(e.target.value)}
+          onChange={(e) => validateField(e.target.value, "Body")}
         ></textarea>
 
         <label>Blog Author:</label>
