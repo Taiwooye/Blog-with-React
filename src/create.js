082c96764501bfe77f4866f8ed5296cab8bec877
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-05-02 13:58:57 +0100

    add new blog

--- src/create.js
+++ src/create.js
@@ -1,51 +1,57 @@
 import { useState } from "react";
 
 const Create = () => {
-const [title, setTitle] = useState('');
-const [body, setBody] = useState('');
-const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const blog ={title, body, author }
+    const blog = { title, body, author };
 
-    console.log(blog)
-}
+    setIsPending(true);
 
-    return ( 
-        <div className="create">
-            <h2>Add a new Blog</h2>
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+  };
 
-            <form onSubmit={handleSubmit}> 
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
 
-                <label >Blog title:</label>
-              <input type="text"
-              required
-              value={title} 
-              onChange={(e)=>setTitle(e.target.value)}
-              />
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
 
-              <label>Blog body:</label>
-              <textarea required
-              value={body}
-              onChange={(e)=>setBody(e.target.value)}
-              ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="miles">miles</option>
+        </select>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
+      </form>
+    </div>
+  );
+};
 
-                <label >Blog Author:</label>
-                <select
-                value={author}
-                onChange={(e)=>setAuthor(e.target.value)}>
-                    <option value="mario">mario</option>
-                    <option value="miles">miles</option>
-                </select>
-                <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
-            </form>
-        </div>
-     );
-}
- 
 export default Create;
