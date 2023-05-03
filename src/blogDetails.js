f6d88b6cefb27dbc0ce321ad88b50d787e2da79a
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-05-03 10:52:36 +0100

    complete

--- src/blogDetails.js
+++ src/blogDetails.js
@@ -8,18 +8,15 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
-  const history = useHistory
+  const history = useHistory();
 
-const handleClick =()=>{
-  fetch('http://localhost:8000/blogs/' + blog.id,{
-    method: 'DELETE'
-  }).then(() => {
-    history.push('/');
-  })
-}
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => history.push("/"));
+  };
 
   return (
-    
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
